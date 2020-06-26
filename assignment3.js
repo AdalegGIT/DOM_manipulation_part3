3c5c9b2a85e875335f51bd33ae455e3f37a5cd0e
Harisree <harisree.n.s@gmail.com>
2020-06-25 20:09:55 -0400

    Updating new assignment

---
+++ assignment3.js
@@ -0,0 +1,6 @@
+// Given is a list of users in data.js
+// Create a html page that displays each user info in a card format using js (refer to the image)
+// Each card should contain the name of the user , a button 'VOTE' and the label saying 0 votes
+// On clicking the vote button the corresponding label in the card should increment the vote count
+// Add a Result button at the end of html page
+// At any point of time clicking the Result button should display a label saying who is the winner with maximum votes
